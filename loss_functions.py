3524585e95d14f0fe8fe356fa13340b20f1b54e7
Raunak Kumar <raunak.kumar17@outlook.com>
2023-04-02 10:45:06 -0400

    minor edits

--- loss_functions.py
+++ loss_functions.py
@@ -2,11 +2,13 @@ from typing import Callable, List
 
 import numpy as np
 
-def create_rademacher_functions(
+def average_of_past_decisions_with_rademacher_signs(
     T : int,
     m : int
 ) -> List[Callable]:
     """
+    f_t(h_t) = w_t * 1/sqrt(m) * \sum_{k=0}^{m-1} x_{t-k}, where w_t is a
+    Rademacher random variable.
     """
 
     rng = np.random.default_rng()
@@ -19,27 +21,17 @@ def create_rademacher_functions(
 
     return l_fn
 
-def functions_0(
+def square_diff_around_fixed_point(
     T : int,
     m : int
 ) -> List[Callable]:
     """
+    f_t(h_t) = \sum_{k=0}^{m-1} (x_{t-k} - 0.3)^2
     """
 
-    rng = np.random.default_rng()
-
-    l_fn = []
-
-    for t in range(T):
-
-        l_coefficient = rng.uniform(-1, 1, size = m)
-        def fn(x):
-            res = 0
-            for i in range(m):
-                # res += l_coefficient[i] * (x[-i] - 0.3)**2
-                res += (x[-i] - 0.3)**2
-            return res
-
-        l_fn.append(fn)
+    l_fn = [
+        lambda x : sum([(x[-i] - 0.3)**2 for i in range(m)]) \
+            for t in range(T)
+    ]
 
     return l_fn
