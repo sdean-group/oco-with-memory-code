778e1e37390e24576c3983688195498029c0af30
Raunak Kumar <raunak.kumar17@outlook.com>
2023-04-02 10:26:10 -0400

    added some more plots and another type of functions

--- loss_functions.py
+++ loss_functions.py
@@ -12,14 +12,33 @@ def create_rademacher_functions(
     rng = np.random.default_rng()
     l_sign = rng.choice([-1, 1], size = T, replace = True)
 
+    l_fn = [
+        lambda x : l_sign[t] * np.average(x[-m:]) * np.sqrt(m) \
+            for t in range(T)
+    ]
+
+    return l_fn
+
+def functions_0(
+    T : int,
+    m : int
+) -> List[Callable]:
+    """
+    """
+
+    rng = np.random.default_rng()
+
     l_fn = []
+
     for t in range(T):
 
-        def fn(x : List[float]):
-            """
-            """
-
-            return l_sign[t] * np.average(x[-m:]) * np.sqrt(m)
+        l_coefficient = rng.uniform(-1, 1, size = m)
+        def fn(x):
+            res = 0
+            for i in range(m):
+                # res += l_coefficient[i] * (x[-i] - 0.3)**2
+                res += (x[-i] - 0.3)**2
+            return res
 
         l_fn.append(fn)
 
